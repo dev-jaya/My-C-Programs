936c13cdf90f225c5548d335e7e6d78cd0fc1ffe
JAYA BRAMHENDRA KUMAR <makamjayabramhendrakumar@gmail.com>
2025-11-07 19:35:14 +0530

    square-root-calculator
    Add simple square root calculator

---
+++ square-root-calculator-193400.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+   unsigned long long int a; 
+   double root;
+   printf("enter a value: ");
+   scanf("%llu",&a);
+   root = sqrt(a);
+   printf("\nroot of given value: %lf",root);
+   getch();
+   }
+   
+   
